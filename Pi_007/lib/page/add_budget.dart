1ca826cae901327e1370e545abc4c497f8fb3e9e
chienhuiiii <64218967+Limchienhui@users.noreply.github.com>
2022-09-29 22:29:14 +0800

    remove debug banner from header and added listview to budget page

---
+++ Pi_007/lib/page/add_budget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AddBudget extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+      body: Container(
+        child: Text('Add Budget',style: TextStyle(fontSize: 40)),)
+
+  );
+}
