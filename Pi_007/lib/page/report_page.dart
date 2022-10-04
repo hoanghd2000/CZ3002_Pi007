578ea711940e1674850794d25854c23314eb11bf
x-aniii <100673066+x-aniii@users.noreply.github.com>
2022-10-04 22:37:07 +0800

    update to report page

--- Pi_007/lib/page/report_page.dart
+++ Pi_007/lib/page/report_page.dart
@@ -17,19 +17,18 @@ class ReportPage extends StatelessWidget{
       Row(
         children: <Widget>[
 
-          SizedBox(width:15),
+          SizedBox(width:25),
           ElevatedButton(
             onPressed: (){
               //display weekly report
             },
             child: Text('View Weekly'),
             style: ElevatedButton.styleFrom(
-              side: BorderSide(color: Colors.black,),
+              foregroundColor: Colors.black, side: BorderSide(color: Colors.black,),
               shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(12)
               ),
-              primary: action_button,  //background
-              onPrimary: Colors.black, //foreground
+              primary: action_button, //foreground
             ),
           ),
           SizedBox(width:15),
@@ -65,7 +64,7 @@ class ReportPage extends StatelessWidget{
 
         ],
       ),
-      SizedBox(height:200),
+      SizedBox(height:240),
 
       Text('   Budget for the month:',style: TextStyle(fontSize:20, fontWeight: FontWeight.bold)),
       SizedBox(height:10),
@@ -75,11 +74,12 @@ class ReportPage extends StatelessWidget{
         backgroundColor: Colors.indigo[100],
         valueColor: AlwaysStoppedAnimation<Color>(Colors.indigo[300],),
       ),
+      Text('350 dollars of 500 dollars used',style: TextStyle(fontSize:15, color: Colors.grey)),
       SizedBox(height:15),
 
       Text('   Budget by category:',style: TextStyle(fontSize:20, fontWeight: FontWeight.bold)),
       SizedBox(height:10),
-      Text('    Food:',style: TextStyle(fontSize:15)),
+      Text('    Food:',style: TextStyle(fontSize:18)),
       SizedBox(height:5),
       new LinearProgressIndicator(
         minHeight: 14.0,
@@ -87,20 +87,22 @@ class ReportPage extends StatelessWidget{
         backgroundColor: Colors.teal[50],
         valueColor: AlwaysStoppedAnimation<Color>(Colors.teal[200],),
       ),
+      Text('200 dollars of 250 dollars used',style: TextStyle(fontSize:15, color: Colors.grey)),
       SizedBox(height:5),
-      Text('    Transport:',style: TextStyle(fontSize:15)),
+      Text('    Transport:',style: TextStyle(fontSize:18)),
       SizedBox(height:5),
       new LinearProgressIndicator(
         minHeight: 14.0,
-        value: 0.8,
+        value: 0.9,
         backgroundColor: Colors.deepOrange[50],
         valueColor: AlwaysStoppedAnimation<Color>(Colors.deepOrange[200],),
       ),
+      Text('170 dollars of 200 dollars used',style: TextStyle(fontSize:15, color: Colors.grey)),
       SizedBox(height:15),
 
       Row(
           children: <Widget>[
-            SizedBox(width:140),
+            SizedBox(width:150),
             ElevatedButton(
         onPressed: (){
           //save report
