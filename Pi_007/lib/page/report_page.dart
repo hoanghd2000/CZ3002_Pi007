caa7c42f687feb5c0af0971e433cd80634ec31ae
x-aniii <100673066+x-aniii@users.noreply.github.com>
2022-10-05 12:50:34 +0800

    updates to report page

--- Pi_007/lib/page/report_page.dart
+++ Pi_007/lib/page/report_page.dart
@@ -8,121 +8,143 @@ class ReportPage extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) => Scaffold(
-    body: ListView(children: <Widget>[
-      Text('Report Page',style: TextStyle(fontSize: 40)),
-      SizedBox(height:15),
-      Text('   Select Time Interval:',style: TextStyle(fontSize:20, fontWeight: FontWeight.bold)),
-      SizedBox(height:10),
+      body: ListView(children: <Widget>[
+        Text('Report Page',style: TextStyle(fontSize: 40)),
+        SizedBox(height:15),
+        Text('   Select Time Interval:',style: TextStyle(fontSize:20, fontWeight: FontWeight.bold)),
+        SizedBox(height:10),
 
-      Row(
-        children: <Widget>[
-
-          SizedBox(width:25),
-          ElevatedButton(
-            onPressed: (){
-              //display weekly report
-            },
-            child: Text('View Weekly'),
-            style: ElevatedButton.styleFrom(
-              foregroundColor: Colors.black, side: BorderSide(color: Colors.black,),
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12)
-              ),
-              primary: action_button, //foreground
-            ),
-          ),
-          SizedBox(width:15),
-          ElevatedButton(
-            onPressed: (){
-              //display monthly report
-            },
-            child: Text('View Monthly'),
-            style: ElevatedButton.styleFrom(
-              side: BorderSide(color: Colors.black,),
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12)
-              ),
-              primary: action_button,  //background
-              onPrimary: Colors.black, //foreground
-            ),
-          ),
-          SizedBox(width:15),
-          ElevatedButton(
-            onPressed: (){
-              //display yearly report
-            },
-            child: Text('View Yearly'),
-            style: ElevatedButton.styleFrom(
-              side: BorderSide(color: Colors.black,),
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12)
-              ),
-              primary: action_button,  //background
-              onPrimary: Colors.black, //foreground
-            ),
-          ),
-
-        ],
-      ),
-      SizedBox(height:240),
-
-      Text('   Budget for the month:',style: TextStyle(fontSize:20, fontWeight: FontWeight.bold)),
-      SizedBox(height:10),
-      new LinearProgressIndicator(
-        minHeight: 14.0,
-        value: 0.6,
-        backgroundColor: Colors.indigo[100],
-        valueColor: AlwaysStoppedAnimation<Color>(Colors.indigo[300],),
-      ),
-      Text('350 dollars of 500 dollars used',style: TextStyle(fontSize:15, color: Colors.grey)),
-      SizedBox(height:15),
-
-      Text('   Budget by category:',style: TextStyle(fontSize:20, fontWeight: FontWeight.bold)),
-      SizedBox(height:10),
-      Text('    Food:',style: TextStyle(fontSize:18)),
-      SizedBox(height:5),
-      new LinearProgressIndicator(
-        minHeight: 14.0,
-        value: 0.8,
-        backgroundColor: Colors.teal[50],
-        valueColor: AlwaysStoppedAnimation<Color>(Colors.teal[200],),
-      ),
-      Text('200 dollars of 250 dollars used',style: TextStyle(fontSize:15, color: Colors.grey)),
-      SizedBox(height:5),
-      Text('    Transport:',style: TextStyle(fontSize:18)),
-      SizedBox(height:5),
-      new LinearProgressIndicator(
-        minHeight: 14.0,
-        value: 0.9,
-        backgroundColor: Colors.deepOrange[50],
-        valueColor: AlwaysStoppedAnimation<Color>(Colors.deepOrange[200],),
-      ),
-      Text('170 dollars of 200 dollars used',style: TextStyle(fontSize:15, color: Colors.grey)),
-      SizedBox(height:15),
-
-      Row(
+        Row(
           children: <Widget>[
-            SizedBox(width:150),
+
+            SizedBox(width:25),
             ElevatedButton(
-        onPressed: (){
-          //save report
-        },
-        child: Text('Save Report'),
-        style: ElevatedButton.styleFrom(
-          side: BorderSide(color: Colors.black,),
-          shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(12)
+              onPressed: (){
+                //display weekly report
+              },
+              child: Text('View Weekly'),
+              style: ElevatedButton.styleFrom(
+                foregroundColor: Colors.black, side: BorderSide(color: Colors.black,),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12)
+                ),
+                primary: action_button, //foreground
+              ),
+            ),
+            SizedBox(width:15),
+            ElevatedButton(
+              onPressed: (){
+                //display monthly report
+              },
+              child: Text('View Monthly'),
+              style: ElevatedButton.styleFrom(
+                side: BorderSide(color: Colors.black,),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12)
+                ),
+                primary: action_button,  //background
+                onPrimary: Colors.black, //foreground
+              ),
+            ),
+            SizedBox(width:15),
+            ElevatedButton(
+              onPressed: (){
+                //display yearly report
+              },
+              child: Text('View Yearly'),
+              style: ElevatedButton.styleFrom(
+                side: BorderSide(color: Colors.black,),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12)
+                ),
+                primary: action_button,  //background
+                onPrimary: Colors.black, //foreground
+              ),
+            ),
+
+          ],
         ),
-          primary: confirm_button,  //background
-          onPrimary: Colors.black, //foreground
+        SizedBox(height:240),
+
+        Text('   Budget for the month:',style: TextStyle(fontSize:20, fontWeight: FontWeight.bold)),
+        SizedBox(height:10),
+        Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: new LinearProgressIndicator(
+            minHeight: 14.0,
+            value: 0.6,
+            backgroundColor: Colors.indigo[100],
+            valueColor: AlwaysStoppedAnimation<Color>(Colors.indigo[300],),
+          ),
         ),
-      ),
+        Text('    350 dollars of 500 dollars used',style: TextStyle(fontSize:15, color: Colors.grey)),
+        SizedBox(height:15),
+
+        Text('   Budget by category:',style: TextStyle(fontSize:20, fontWeight: FontWeight.bold)),
+        SizedBox(height:10),
+        Text('    Food:',style: TextStyle(fontSize:18)),
+        SizedBox(height:5),
+        Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: new LinearProgressIndicator(
+            minHeight: 14.0,
+            value: 0.8,
+            backgroundColor: Colors.teal[50],
+            valueColor: AlwaysStoppedAnimation<Color>(Colors.teal[200],),
+          ),
+        ),
+        Text('    200 dollars of 250 dollars used',style: TextStyle(fontSize:15, color: Colors.grey)),
+        SizedBox(height:5),
+        Text('    Transport:',style: TextStyle(fontSize:18)),
+        SizedBox(height:5),
+        Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: new LinearProgressIndicator(
+            minHeight: 14.0,
+            value: 0.9,
+            backgroundColor: Colors.deepOrange[50],
+            valueColor: AlwaysStoppedAnimation<Color>(Colors.deepOrange[200],),
+          ),
+        ),
+        Text('    170 dollars of 200 dollars used',style: TextStyle(fontSize:15, color: Colors.grey)),
+        SizedBox(height:15),
+
+        Row(
+            children: <Widget>[
+              SizedBox(width:150),
+              ElevatedButton(
+                onPressed: (){
+                  showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      title: Text('Report saved successfully.'),
+                        actions: [
+                          TextButton(
+                            child: Text('OK'),
+                            onPressed: () => Navigator.pop(context),
+
+                    ),
+                      ],
+                    )
+                  );
+                },
+                child: Text('Save Report'),
+                style: ElevatedButton.styleFrom(
+                  side: BorderSide(color: Colors.black,),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12)
+                  ),
+                  primary: confirm_button,  //background
+                  onPrimary: Colors.black, //foreground
+
+                ),
+              ),
 
 
-    ]
-  )
-  ]
-    )
+            ]
+        )
+      ]
+      )
   );
 
-  }
+}
